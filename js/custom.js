5b3c220125b8065956d631f6b6e4cdb58ad31b7c
kajol <mahirsarkar2001@gmail.com>
2023-06-01 18:00:23 +0600

    tab content

--- js/custom.js
+++ js/custom.js
@@ -403,3 +403,22 @@
 
 
 })(jQuery);
+
+//create tabs
+var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
+var tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
+
+function showPanel(panelIndex,colorCode) {
+    tabButtons.forEach(function(node){
+        node.style.backgroundColor="";
+        node.style.color="";
+    });
+    tabButtons[panelIndex].style.backgroundColor=colorCode;
+    tabButtons[panelIndex].style.color="black";
+    tabPanels.forEach(function(node){
+        node.style.display="none";
+    });
+    tabPanels[panelIndex].style.display="block";
+    tabPanels[panelIndex].style.backgroundColor=colorCode;
+}
+showPanel(0,'');
